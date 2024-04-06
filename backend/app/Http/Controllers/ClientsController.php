642d0cd3afdf3f28c48a53365015d790a2c6fdcb
repoPrasanna <prasanna.repo@gmail.com>
2024-04-06 17:37:07 +0545

    asdf

--- backend/app/Http/Controllers/ClientsController.php
+++ backend/app/Http/Controllers/ClientsController.php
@@ -17,18 +17,14 @@ class ClientsController extends Controller
 
     public function __construct(ClientInterface $clientRepository)             {
         $this->clientRepository = $clientRepository;
-    
-    
-    
-    
-                  }
+    }
 
     public function index(): JsonResponse
     {
         try {
             return $this->responseSuccess($this->clientRepository->all(), 'Partners fetched successfully.');
         } catch (Exception $e) {
-            Log::info($e->getMessage().' | File: '.__FILE__.' | Line: '.__LINE__);
+            Log::info($e->getMessage() . ' | File: ' . __FILE__ . ' | Line: ' . __LINE__);
 
             return $this->responseError();
         }
@@ -37,13 +33,13 @@ class ClientsController extends Controller
     public function store(StoreClientRequest $request)
     {
         try {
-            if (! $this->clientRepository->checkIfFileExists()) {
+            if (!$this->clientRepository->checkIfFileExists()) {
                 $this->clientRepository->createFile();
             }
 
             return $this->responseSuccess($this->clientRepository->store($request->all()), 'Partners added successfully.');
         } catch (Exception $e) {
-            Log::info($e->getMessage().' | File: '.__FILE__.' | Line: '.__LINE__);
+            Log::info($e->getMessage() . ' | File: ' . __FILE__ . ' | Line: ' . __LINE__);
 
             return $this->responseError();
         }
