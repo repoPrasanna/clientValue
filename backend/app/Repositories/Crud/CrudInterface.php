0625da5b74d8b65848178db559b060ae0af0c09c
repoPrasanna <prasanna.repo@gmail.com>
2024-04-06 15:34:45 +0545

    feat: test travis

--- backend/app/Repositories/Crud/CrudInterface.php
+++ backend/app/Repositories/Crud/CrudInterface.php
@@ -6,6 +6,5 @@ use Illuminate\Http\JsonResponse;
 
 interface CrudInterface
 {
-
     public function store($data): JsonResponse;
 }
--- backend/app/Repositories/Crud/CrudRepository.php
+++ backend/app/Repositories/Crud/CrudRepository.php
@@ -10,8 +10,9 @@ abstract class CrudRepository implements CrudInterface
     public function store($client): JsonResponse
     {
         $csvFileName = 'clients.csv';
-        $csvContent = $client['name'].",".$client['email'];
+        $csvContent = $client['name'].','.$client['email'];
         Storage::disk('csv')->append($csvFileName, $csvContent);
+
         return response()->json(['data' => $csvContent]);
     }
 }
